88abd47e9c0ab47defa1c94feb1b8c869770d1f0
RMD125 <ramdaneagbagni20@gmail.com>
2025-09-12 05:19:13 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    // Numéro du propriétaire/bot
+    ownerNumber: '22896190934@s.whatsapp.net',
+    botNumber: '22896190934@s.whatsapp.net',
+    
+    // Options du bot
+    botName: 'RMD BOT 🤖',
+    ownerName: 'RMD',
+    
+    // Options de messages
+    welcomeMessage: 'Bienvenue dans le groupe!',
+    goodbyeMessage: 'Au revoir!',
+    
+    // Options de sécurité
+    antiSpam: true,
+    antiLink: false,
+    
+    // Options de fonctionnalités
+    autoRead: false,
+    autoTyping: false,
+    
+    // Chemins des fichiers
+    databasePath: './data/database.json',
+    sessionPath: './data/auth_info'
+};
